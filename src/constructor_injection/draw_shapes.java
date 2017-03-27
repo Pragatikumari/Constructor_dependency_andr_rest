925270d4b2c9d0c551855d8e4f0812e8a0c427e2
pragati <pragatisharma062 @gmail.com>
2017-03-27 23:04:21 +0530

    intialisation using Constructor Injection

---
+++ src/constructor_injection/draw_shapes.java
@@ -0,0 +1,19 @@
+package constructor_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class draw_shapes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+       Triangle triangle= (Triangle)context.getBean("triangle");
+       triangle.draw();
+       
+       
+       
+
+	}
+
+}
