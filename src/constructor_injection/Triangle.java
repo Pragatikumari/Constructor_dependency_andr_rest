d03d63dc5780d0b3ab80f6d86cdb423e1e1f8436
pragati <pragatisharma062 @gmail.com>
2017-03-27 23:12:30 +0530

    2 variables are intialised using constructor injection

--- src/constructor_injection/Triangle.java
+++ src/constructor_injection/Triangle.java
@@ -2,24 +2,43 @@ package constructor_injection;
 
 public class Triangle {
 	private String type;
+	private int height;  
+	
 
 	public Triangle(String type)
 	{
 		this.type= type;
 	}
 	
+	public Triangle( String type, int height)
+	{
+		this.type= type;
+		this.height= height;
+	}
+	
 	public String getType() {
 		return type;
 	}
 
+	
+	public int getHeight() {
+		return height;
+	}
+	
 	/*public void setType(String type) {
 		this.type = type;
 	}*/
 	
 	public void draw(){
-		System.out.println(getType()+" Triangle drawn");
+		System.out.println(getType()+" Triangle drawn of height "+getHeight());
 		
 	}
+
+	
+
+	/*public void setHeight(int height) {
+		this.height = height;
+	}*/
 	
 	
 
